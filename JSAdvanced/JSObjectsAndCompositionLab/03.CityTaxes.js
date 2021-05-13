7e04835b2e566efdc3dc947a190d23646911824b
nradkova <neliradkovaa@gmail.com>
2021-05-13 23:36:04 +0300

    Assignments

---
+++ JSAdvanced/JSObjectsAndCompositionLab/03.CityTaxes.js
@@ -0,0 +1,38 @@
+function solve(...input) {
+
+    let [name, population, treasury] = input;
+
+    let city = { name, population, treasury };
+    city.taxRate = 10;
+    city.collectTaxes = collectTaxes;
+    city.applyGrowth = applyGrowth;
+    city.applyRecession = applyRecession;
+
+    // city.collectTaxes();
+    // console.log(city.treasury);
+    // city.applyGrowth(5);
+    // console.log(city.population);
+    // city.applyRecession(5);
+    // console.log(city.treasury);
+
+    return city;
+
+    function collectTaxes() {
+        this.treasury += this.population * this.taxRate;
+        this.treasury=Math.round(this.treasury);
+    }
+
+    function applyGrowth(percentage) {
+        this.population +=this.population / 100 * percentage;
+        this.population=Math.round(this.population);
+    }
+    
+    function applyRecession(percentage) {
+        this.treasury -=this.treasury / 100 * percentage;
+        this.treasury=Math.round(this.treasury);
+    }
+}
+
+console.log(solve('Tortuga',
+    7000,
+    15000));

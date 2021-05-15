cd7a556b8c62de85fd50baea02607b52c73b13ac
nradkova <neliradkovaa@gmail.com>
2021-05-16 00:33:50 +0300

    OpenCourse

---
+++ JSAdvanced/JSObjectsAndCompositionExercise/01.CalorieObject.js
@@ -0,0 +1,13 @@
+function solve(input) {
+    return input.reduce((acc, el, i) => {
+        if (i % 2 === 0) {
+            acc[el] = undefined;
+        } else {
+            let prevEl = input[i - 1];
+            acc[prevEl] = Number(el);
+        }
+        return acc;
+    }, {});
+}
+
+console.log(solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));

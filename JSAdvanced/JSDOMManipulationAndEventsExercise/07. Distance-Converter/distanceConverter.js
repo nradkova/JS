384a8c4fe5b7fc00b9e1399d0a161ba346002450
nradkova <neliradkovaa@gmail.com>
2021-05-25 11:43:44 +0300

    Assignments

--- JSAdvanced/JSDOMManipulationAndEventsExercise/07. Distance-Converter/distanceConverter.js
+++ JSAdvanced/JSDOMManipulationAndEventsExercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,24 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    document.querySelector('#convert').addEventListener('click', onClick);
+   
+    function onClick(event) {
+        document.querySelector('#outputDistance').disabled = false;
+        const meterBase = {
+            km: 1000,
+            m: 1,
+            cm: 0.01,
+            mm: 0.001,
+            mi: 1609.34,
+            yrd: 0.9144,
+            ft: 0.3048,
+            in: 0.0254
+        };
+        const inputType = document.querySelectorAll('select')[0].value;
+        const inputValue = Number(document.querySelector('#inputDistance').value);
+        const outputType = document.querySelectorAll('select')[1].value;
+
+       const outputValue=inputValue* meterBase[inputType]*meterBase.m/meterBase[outputType];
+       
+       document.querySelector('#outputDistance').value = outputValue;
+    }
 }

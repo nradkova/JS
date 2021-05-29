99cf11ad2be6918271beac0cd83c3de5043eb273
nradkova <neliradkovaa@gmail.com>
2021-05-29 16:14:52 +0300

    Assignments

---
+++ JSAdvanced/JSAdvancedFunctionsExercise/10. Task Manager/app.js
@@ -0,0 +1,93 @@
+function solve() {
+    const [add, open, inProgress, complete] = Array.from(document.querySelector('div').children);
+    const [taskTag, descriptionTag, dateTag]=
+        Array.from(add.children[1].children[0].children)
+             .filter(x => x.tagName === 'INPUT' || x.tagName === 'TEXTAREA');
+
+    const manager = {
+        addFunc,
+        startFunc,
+        deleteFunc,
+        finishFunc
+    };
+
+    document.querySelector('div').addEventListener('click', (event) => {
+        event.preventDefault();
+        const command = event.target.textContent.toLowerCase() + 'Func';
+        if (manager[command]){
+             manager[command](event.target);
+        }});
+
+    function finishFunc(currentBtn) {
+        const article = currentBtn.parentNode.parentNode;
+        complete.children[1].appendChild(article);
+        currentBtn.parentNode.remove();
+    }
+
+    function deleteFunc(currentBtn) {
+        currentBtn.parentNode.parentNode.remove();
+    }
+
+    function startFunc(currentBtn) {
+        const article = currentBtn.parentNode.parentNode;
+        inProgress.children[1].appendChild(article);
+        const [first, second] = Array.from(article.querySelectorAll('button'));
+
+        first.textContent = 'Delete';
+        first.classList.remove('green');
+        first.classList.add('red');
+
+        second.textContent = 'Finish';
+        second.classList.remove('red');
+        second.classList.add('orange');
+    }
+
+    function addFunc(currentBtn) {
+        const form = currentBtn.parentNode;
+        if (taskTag.value === ''
+            || descriptionTag.value === ''
+            || dateTag.value === '') {
+            return;
+        }
+        const task = taskTag.value;
+        const description = descriptionTag.value;
+        const date = dateTag.value;
+
+        taskTag.value = '';
+        descriptionTag.value = '';
+        dateTag.value = '';
+
+        const article = createArticle(task, description, date);
+        open.children[1].appendChild(article);
+    }
+
+    function createArticle(task, description, date) {
+        const article = createCustomEl('article');
+        article.appendChild(createCustomEl('h3', task));
+        article.appendChild(createCustomEl('p', `Description: ${description}`));
+        article.appendChild(createCustomEl('p', `Due Date: ${date}`));
+
+        const div = createCustomEl('div');
+        div.classList.add('flex')
+
+        const stBtn = createCustomEl('button', 'Start');
+        stBtn.classList.add('green');
+        div.appendChild(stBtn);
+
+        const delBtn = createCustomEl('button', 'Delete');
+        delBtn.classList.add('red');
+        div.appendChild(delBtn);
+
+        article.appendChild(div);
+        return article;
+    }
+
+    function createCustomEl(type, content) {
+        const el = document.createElement(type);
+        if (!content) { return el; }
+        type === 'input'
+            ? el.value = content
+            : el.textContent = content;
+        return el;
+    }
+}

e596ee0e279183a0c846acac59958b75cb7e546b
nradkova <neliradkovaa@gmail.com>
2021-06-14 07:43:35 +0300

    Assignments

--- JSAdvanced/JSUnitTestingAndErrorHandlingLab/06.rgb-to-hex/rgbToHex.test.js
+++ JSAdvanced/JSUnitTestingAndErrorHandlingLab/06.rgb-to-hex/rgbToHex.test.js
@@ -1,7 +1,7 @@
 const rgbToHexColor = require('./rgbToHex');
 const assert = require("chai").assert;
 
-describe('check rgbToHexColor functionality', () => {
+describe('check rgbToHexColor  functionality', () => {
 
     it('return black to hex', () => {
         assert.equal('#000000',rgbToHexColor(0, 0, 0));
@@ -17,37 +17,17 @@ describe('check rgbToHexColor functionality', () => {
 
     it('negative input values return undefined', () => {
         assert.isUndefined(rgbToHexColor(-1,0,0))
-    });
-
-    it('negative input values return undefined', () => {
         assert.isUndefined(rgbToHexColor(0,-1,0))
-    });
-
-    it('negative input values return undefined', () => {
         assert.isUndefined(rgbToHexColor(0,0,-1))
     });
-
     it('greater than 255 input values return undefined', () => {
         assert.isUndefined(rgbToHexColor(256,0,0))
-    });
-
-    it('greater than 255 input values return undefined', () => {
         assert.isUndefined(rgbToHexColor(0,256,0))
-    });
-
-    it('greater than 255 input values return undefined', () => {
         assert.isUndefined(rgbToHexColor(0,0,256))
     });
-
     it('invalid type input values return undefined', () => {
         assert.isUndefined(rgbToHexColor('a',0,0))
-    });
-    
-    it('invalid type input values return undefined', () => {
         assert.isUndefined(rgbToHexColor(0,'a',0))
-    });
-    
-    it('invalid type input values return undefined', () => {
         assert.isUndefined(rgbToHexColor(0,0,'a'))
     });
 });
